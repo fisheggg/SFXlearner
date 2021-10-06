90f8b7fb3fea1773afd8e0b68a75a214e7380e0f
Arthur Guo <jg5505@nyu.edu>
2021-10-05 22:12:17 -0400

    Update multiFX data generation

--- src/generate_data.py
+++ src/generate_data.py
@@ -10,6 +10,7 @@ import sox
 import soundfile as sf
 import attr
 import numpy as np
+from sox import transform
 import torch
 from torch.serialization import validate_cuda_device
 import yaml
@@ -17,7 +18,7 @@ import pprint
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 from collections import OrderedDict
-from itertools import combinations
+from itertools import combinations, product
 
 def slice_guitarset(data_home, save_dir, duration=5):
     """
@@ -84,7 +85,6 @@ def slice_guitarset(data_home, save_dir, duration=5):
                 file_count += 1
     
     print(f"Generation complete! {file_count} audio files is generated to {os.path.join(save_dir, folder_name)}.")
-
 
 def gen_singleFX_1on1(clean_dirs: list, 
                       output_dir: str, 
@@ -379,7 +379,7 @@ def gen_singleFX_1onN(clean_dirs: list,
         'sample_rate': 44100,
         'random_seed': random_seed,
         'n_classes': len(fx_params),
-        'add_bypass_class': add_bypass_class
+        'add_bypass_class': add_bypass_class,
     }
 
     # generation start
@@ -530,13 +530,13 @@ def gen_singleFX_1onN(clean_dirs: list,
     print(f"=> settings written to {os.path.join(output_full_path, 'settings.yml')}")
     print("=> Done!")
 
-def gen_multiFX(clean_dirs: list, 
-                output_dir: str, 
+def gen_multiFX(clean_dirs: list,
+                output_dir: str,
                 methods: list,
                 fx_params: dict = None,
                 grouping: list = None,
-                normalize: bool = False, 
-                random_seed: int = None, 
+                normalize: bool = False,
+                random_seed: int = 42,
                 duration: float = 5,
                 valid_split: float = 0.2,
                 add_bypass_class: bool = False):
@@ -574,11 +574,12 @@ def gen_multiFX(clean_dirs: list,
     ----------
     clean_dirs: list of dirs, the audio sources
     output_dir: dir of output samples, subfolder will be created.
-    methods: list of int, how the effect chains are generated.
+    methods: list of int or strings, how the effect chains are generated.
              int N will apply N groups of effects. 
                 It will iterate over all combinations of groups, and all effects within a group.
              can have multiple Ns.
-             N must in [1, n_groups].             
+             N must in [1, n_groups].
+             valid strings: "random"
     fx_params: dict of fx types and parameters. if not set, use the defaul fx list.
                 each dict represents an FX group.
     grouping: list of tuples, indicates the grouping of effects.
@@ -642,7 +643,8 @@ def gen_multiFX(clean_dirs: list,
         'fx_chain_type': 'multi',
         'generation_type': methods,
         'origins': clean_dirs,
-        'size': 0,
+        'train_size': 0,
+        'valid_size': 0,
         'fx_params': OrderedDict(fx_params),
         'grouping': grouping,
         'n_groups': n_groups,
@@ -651,31 +653,187 @@ def gen_multiFX(clean_dirs: list,
         'sample_rate': 44100,
         'random_seed': random_seed,
         'n_classes': len(fx_params),
-        'add_bypass_class': add_bypass_class
+        'add_bypass_class': add_bypass_class,
     }
-    for apply_n in methods:
-        assert apply_n >= 1 and apply_n <= n_groups
+    for method in methods:
+        if type(method) == int:
+            assert method >= 1 and method <= n_groups
+        elif type(method) == str:
+            assert method == "random"
+        else:
+            raise ValueError(f"Invalid method: {method}")
 
     # generation start
     output_full_path = os.path.join(output_dir, f"gen_multiFX_{datetime.now().strftime('%m%d%Y')}")
     os.mkdir(output_full_path)
     print("=> Settings:")
     pprint.pprint(settings)
-    sample_count = 0
-    labels = [] # -1 is clean, others according to fx list
-    clean_link = []
+    train_sample_count = 0
+    valid_sample_count = 0
+    train_labels = [] # shape: (n_samples, n_classes)
+    valid_labels = []
+    train_clean_link = []
+    valid_clean_link = []
     if random_seed:
         random.seed(random_seed)
 
     print("=> Initializing sox transformers")
     transformers = []
-    for apply_n in methods:
-        for groups_to_apply in combinations(range(n_groups), apply_n):
+    transformers_labels = [] #shape: (len(transformers), n_classes)
+    fx_name_list = list(fx_params.keys())
+    for method in methods:
+        if type(method) is int:
+            for groups_to_apply in combinations(range(n_groups), method):
+                # for one method N, find all the combinations of groups
+                # e.g N=5, then groups_to_apply=((0,1,2,3,4),(0,1,2,3,5),(0,1,2,4,5),(0,1,3,4,5),(0,2,3,4,5),(1,2,3,4,5))
+                fx_chain_iter = product(*list(map(lambda x: grouping[x], groups_to_apply)))
+                # for one fixed groups_to_apply, get all the combinations of fx. Iterates over all fx combinations amoung groups
+                # e.g. groups_to_apply = (0,1,2), then fx_chain_iter=((0,2,3),(0,2,4),(1,2,3),(1,2,4))
+                for fx_chain in fx_chain_iter:
+                    transformers.append(sox.transform.Transformer())
+                    label = [0]*len(fx_params)
+                    for i in fx_chain:
+                        label[i] = 1
+                    transformers_labels.append(label)
+                    for i in fx_chain:
+                        fx = fx_name_list[i]
+                        apply_fx_to_transformer(transformers[-1], fx, fx_params)
+                        transformers[-1].trim(start_time=0, end_time=duration)
+                        transformers[-1].set_output_format(bits=16)
+        elif method == "random":
+            #TODO
             pass
-            
+
+    print("=> Rendering audio files")
+
+    train_audio_dir = os.path.join(output_full_path, 'train/audio/')
+    valid_audio_dir = os.path.join(output_full_path, 'valid/audio/')
+    os.makedirs(train_audio_dir)
+    os.makedirs(valid_audio_dir)
+    for clean_dir in clean_dirs:
+        clean_paths = os.listdir(clean_dir)
+        train_paths, valid_paths = train_test_split(clean_paths, 
+                                                   test_size=valid_split, 
+                                                   random_state=random_seed,
+                                                   shuffle=True)
+        settings['train_size'] += len(train_paths) * (len(transformers))
+        settings['valid_size'] += len(valid_paths) * (len(transformers))
+
+        print(f"=> Generating training set from {clean_dir}")
+        for sample in tqdm(train_paths):
+            clean_sample_path = os.path.join(clean_dir, sample)
+            sample_audio, sr = sf.read(clean_sample_path)
+            if sr != 44100:
+                raise ValueError(f"Invalid sample rate: {sr}. Dataset sample rate: {settings['sample_rate']}.")
+            if normalize:
+                sample_audio = librosa.util.normalize(sample_audio)
+            # randomly apply an fx to the sample
+            if add_bypass_class:
+                i_range = range(-1, len(transformers))
+            else:
+                i_range = range(0, len(transformers))
+            for i in tqdm(i_range):
+                train_clean_link.append(clean_sample_path)
+                train_labels.append(transformers_labels[i])
+                output_file_name = os.path.join(train_audio_dir, f"{train_sample_count}.wav")
+                if i == -1:
+                    sf.write(output_file_name, sample_audio, settings['sample_rate'])
+                else:
+                    transformers[i].build_file(input_array=sample_audio, sample_rate_in=settings['sample_rate'],
+                                           output_filepath=output_file_name)
+                train_sample_count += 1
+        print(f"=> Generating valid set from {clean_dir}")
+        for sample in tqdm(valid_paths):
+            clean_sample_path = os.path.join(clean_dir, sample)
+            sample_audio, sr = sf.read(clean_sample_path)
+            if sr != 44100:
+                raise ValueError(f"Invalid sample rate: {sr}. Dataset sample rate: {settings['sample_rate']}.")
+            if normalize:
+                sample_audio = librosa.util.normalize(sample_audio)
+            # randomly apply an fx to the sample
+            if add_bypass_class:
+                i_range = range(-1, len(transformers))
+            else:
+                i_range = range(0, len(transformers))
+            for i in tqdm(i_range):
+                valid_clean_link.append(clean_sample_path)
+                valid_labels.append(transformers_labels[i])
+                output_file_name = os.path.join(valid_audio_dir, f"{valid_sample_count}.wav")
+                if i == -1:
+                    sf.write(output_file_name, sample_audio, settings['sample_rate'])
+                else:
+                    transformers[i].build_file(input_array=sample_audio, sample_rate_in=settings['sample_rate'],
+                                           output_filepath=output_file_name)
+                valid_sample_count += 1
+
+    assert train_sample_count == settings['train_size']
+    assert valid_sample_count == settings['valid_size']
+    print(f"=> Generated {train_sample_count} samples to {output_full_path}/train")
+    print(f"=> Generated {valid_sample_count} samples to {output_full_path}/valid")
+
+    train_label_tensor = torch.tensor(train_labels)
+    valid_label_tensor = torch.tensor(valid_labels)
+    torch.save(train_label_tensor, os.path.join(f"{output_full_path}/train", "label_tensor.pt"))
+    print(f"=> Training set labels written to {os.path.join(output_full_path, 'train/', 'label_tensor.pt')}")
+    torch.save(valid_label_tensor, os.path.join(f"{output_full_path}/valid", "label_tensor.pt"))
+    print(f"=> Validation set labels written to {os.path.join(output_full_path, 'valid/', 'label_tensor.pt')}")
+
+    with open(os.path.join(output_full_path, 'train/', 'clean_link.csv'), 'w') as outfile:
+        for sample in train_clean_link:
+            outfile.write(sample)
+            outfile.write('\n')
+    print(f"=> Training link file written to {os.path.join(output_full_path, 'train/', 'clean_link.csv')}")
+    with open(os.path.join(output_full_path, 'valid/', 'clean_link.csv'), 'w') as outfile:
+        for sample in valid_clean_link:
+            outfile.write(sample)
+            outfile.write('\n')
+    print(f"=> Validation link file written to {os.path.join(output_full_path, 'valid/', 'clean_link.csv')}")
+
+    with open(os.path.join(output_full_path, 'settings.yml'), 'w') as outfile:
+        yaml.dump(settings, outfile, default_flow_style=False)
+    print(f"=> settings written to {os.path.join(output_full_path, 'settings.yml')}")
+    print("=> Done!")
 
 
-
+def apply_fx_to_transformer(transformer, fx, fx_params):
+    """
+    Append the corresponding fx to the given transformer.
+    """
+    assert isinstance(transformer, sox.transform.Transformer)
+    if fx == 'distortion':
+        transformer.overdrive(**fx_params[fx]) # params: {'param1': val1, 'param2', val2}
+    elif fx == 'overdrive':
+        transformer.overdrive(**fx_params[fx])
+    elif fx == 'feedback_delay':
+        transformer.echos(**fx_params[fx])
+    elif fx == 'slapback_delay':
+        transformer.echo(**fx_params[fx])
+    elif fx == 'reverb':
+        transformer.reverb(**fx_params[fx])
+    elif fx == 'chorus':
+        transformer.chorus(**fx_params[fx])
+    elif fx == 'flanger':
+        transformer.flanger(**fx_params[fx])
+    elif fx == 'phaser':
+        transformer.phaser(**fx_params[fx])
+    elif fx == 'tremolo':
+        transformer.tremolo(**fx_params[fx])
+    elif fx == 'vibrato':
+        raise NotImplementedError(fx)
+    elif fx == 'low_boost':
+        transformer.bass(**fx_params[fx])
+    elif fx == 'low_reduct':
+        transformer.bass(**fx_params[fx])
+    elif fx == 'mid_boost':
+        transformer.equalizer(**fx_params[fx])
+    elif fx == 'mid_reduct':
+        transformer.equalizer(**fx_params[fx])
+    elif fx == 'hi_boost':
+        transformer.treble(**fx_params[fx])
+    elif fx == 'hi_reduct':
+        transformer.treble(**fx_params[fx])
+    else:
+        raise ValueError(f"Invalid effect name: {fx}")
 
 
 if __name__ == "__main__":
@@ -683,4 +841,5 @@ if __name__ == "__main__":
     # slice_guitarset(data_home=data_home, save_dir="./dataset", duration=5)
     # gen_singleFX_1on1(["dataset/clean/guitarset_5s"], "dataset/generated")
     # gen_singleFX_1onN(["dataset/clean/guitarset10"], "dataset/generated")
-    gen_singleFX_1onN(["dataset/clean/guitarset_5s"], "dataset/generated")
+    # gen_singleFX_1onN(["dataset/clean/guitarset_5s"], "dataset/generated")
+    gen_multiFX(["dataset/clean/guitarset10"], "dataset/generated", [5])
